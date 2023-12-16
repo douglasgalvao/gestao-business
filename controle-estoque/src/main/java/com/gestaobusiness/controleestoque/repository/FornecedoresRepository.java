481633d116236dc048ceee9abee41ed52304db01
douglas <douglasgmachado98@gmail.com>
2023-12-16 15:32:35 -0300

    create Fornecedor Repository

--- controle-estoque/src/main/java/com/gestaobusiness/controleestoque/repository/FornecedoresRepository.java
+++ controle-estoque/src/main/java/com/gestaobusiness/controleestoque/repository/FornecedoresRepository.java
@@ -2,8 +2,8 @@ package com.gestaobusiness.controleestoque.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.gestaobusiness.controleestoque.models.Fornecedores;
+import com.gestaobusiness.controleestoque.models.Fornecedor;
 
-public interface FornecedoresRepository extends JpaRepository<Fornecedores, Long> {
+public interface FornecedoresRepository extends JpaRepository<Fornecedor, Long> {
 
 }
