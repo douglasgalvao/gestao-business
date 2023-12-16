39f3defdb22585ce4019b4d5415cad025b842a28
douglas <douglasgmachado98@gmail.com>
2023-12-16 15:44:32 -0300

    feat: create compras repository

---
+++ controle-estoque/src/main/java/com/gestaobusiness/controleestoque/repository/CompraRepository.java
@@ -0,0 +1,9 @@
+package com.gestaobusiness.controleestoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gestaobusiness.controleestoque.models.Compra;
+
+public interface CompraRepository extends JpaRepository<Compra, Long> {
+
+}
