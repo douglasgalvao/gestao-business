d0a8e3fbd3f3730510696933eb59ae0a523a2035
douglas <douglasgmachado98@gmail.com>
2023-12-17 02:55:18 -0300

    fix: add bat in desktop to initiate the server

--- controle-estoque/run-app.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -jar controle-estoque-0.0.1-SNAPSHOT.jar
---
+++ controle-estoque/src/main/java/com/gestaobusiness/controleestoque/models/gestao-server.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar .\Development\gestao-business-backend\controle-estoque\target\controle-estoque-0.0.1-SNAPSHOT.jar
