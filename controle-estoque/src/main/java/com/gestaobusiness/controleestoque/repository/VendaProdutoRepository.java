3481cf3b2b90b40648909b8182c3555229e7f419
douglas <douglasgmachado98@gmail.com>
2023-12-18 12:56:40 -0300

    feat: add view vendas_produtos in db & return list<tuple>(vdprd)

--- controle-estoque/src/main/java/com/gestaobusiness/controleestoque/repository/VendaProdutoRepository.java
+++ controle-estoque/src/main/java/com/gestaobusiness/controleestoque/repository/VendaProdutoRepository.java
@@ -1,9 +1,15 @@
 package com.gestaobusiness.controleestoque.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.gestaobusiness.controleestoque.models.VendaProduto;
 
-public interface VendaProdutoRepository extends JpaRepository<VendaProduto, Long> {
+import jakarta.persistence.Tuple;
 
+public interface VendaProdutoRepository extends JpaRepository<VendaProduto, Long> {
+    @Query(value = "select * from public.\"vendas_produtos_VIEW\"", nativeQuery = true)
+    List<Tuple> findAllVendaProdutosTuples();
 }
