de0cf9a8465199d8f24b302ed268a8c7993940c3
douglas <douglasgmachado98@gmail.com>
2024-01-04 16:19:37 -0300

    remove: imports it doesn't used

--- controle-estoque/src/main/java/com/gestaobusiness/controleestoque/configs/WebConfig.java
+++ controle-estoque/src/main/java/com/gestaobusiness/controleestoque/configs/WebConfig.java
@@ -1,12 +1,14 @@
 package com.gestaobusiness.controleestoque.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Order(0)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
