1f7182380e2fe59d2d6ebb2ed9287018c0c8f96f
douglas <douglasgmachado98@gmail.com>
2024-01-04 16:16:20 -0300

    remove: exlude of Auto config Security Spring class

--- controle-estoque/src/main/java/com/gestaobusiness/controleestoque/ControleEstoqueApplication.java
+++ controle-estoque/src/main/java/com/gestaobusiness/controleestoque/ControleEstoqueApplication.java
@@ -2,9 +2,8 @@ package com.gestaobusiness.controleestoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class ControleEstoqueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ControleEstoqueApplication.class, args);
