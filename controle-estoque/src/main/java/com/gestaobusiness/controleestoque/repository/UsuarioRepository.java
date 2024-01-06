c1e73acc817bbee07d31ef523587999f2a559be8
douglas <douglasgmachado98@gmail.com>
2024-01-06 06:32:06 -0300

    chore: delete Usuario is not used, now is used User that implements UserDetails of Spring

--- controle-estoque/src/main/java/com/gestaobusiness/controleestoque/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestaobusiness.controleestoque.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gestaobusiness.controleestoque.models.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
